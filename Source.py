9e698ca996b79fea171db9c05c2a2ba2bedd2f86
kazimierczak-robert <kazimierczak-robert@tlen.pl>
2018-12-12 19:28:56 +0100

    Refactoring

--- Source.py
+++ Source.py
@@ -7,14 +7,14 @@ import tensorflow as tf
 
 
 class OneHot:
-    one_hot_colours: list
-    one_hot_figures: list
-    one_hot_hand: list
+    one_hot_suits: list
+    one_hot_ranks: list
+    one_hot_hands: list
 
     def __init__(self):
-        self.one_hot_colours = self.get_one_hot_vector(4)
-        self.one_hot_figures = self.get_one_hot_vector(13)
-        self.one_hot_hand = self.get_one_hot_vector(10)
+        self.one_hot_suits = self.get_one_hot_vector(4)
+        self.one_hot_ranks = self.get_one_hot_vector(13)
+        self.one_hot_hands = self.get_one_hot_vector(10)
 
     @staticmethod
     def get_one_hot_vector(set_stop):
@@ -22,57 +22,36 @@ class OneHot:
 
 
 class TrainingSetPreparer:
+    # Hands:
+    # 1: Royal Straight Flush
+    # 2: Straight Flush
+    # 3: Four of a Kind
+    # 4: Full House
+    # 5: Flush
+    # 6: Straight
+    # 7: Three of a Kind
+    # 8: Two Pair
+    # 9: Pair
+    # 10: High Card
+
+    # Suits:
+    # 1: Spades (s)
+    # 2: Hearts (h)
+    # 3: Diamonds (d)
+    # 4: Clubs (c)
+
+    # Ranks:
+    # 1 : 2 (analogically for ranks 3, 4, ..., 9)
+    # 9: 10 (T)
+    # 10: Jack (J)
+    # 11: Queen (Q)
+    # 12: King (K)
+    # 13: Ace (A)
+
     evaluator = Evaluator()
     generated_probes = []
 
-    # Prepare files for 4 cases with card_parameter: 2, 5, 6, 7
-    # 2 card in hand 0 visible on table (2)
-    # 2 card in hand 3 visible on table (5)
-    # 2 card in hand 4 visible on table (6)
-    # 2 card in hand 5 visible on table (7)
-    def generate_training_set(self, on_board_parameter):
-        if all([on_board_parameter >= 3, on_board_parameter <= 5]):
-            # training set
-            with open("poker-hand-training-"+str(on_board_parameter+2)+".data", "w") as training_set:
-                for i in range(0, 4):  # 32768
-                    training_set.write(self.generate_probe(on_board_parameter)+"\n")
-                    training_set.flush()
-            # test set
-            with open("poker-hand-test-"+str(on_board_parameter+2)+".data", "w") as test_set:
-                for i in range(0, 2):  # 4096
-                    test_set.write(self.generate_probe(on_board_parameter)+"\n")
-                    test_set.flush()
-
-        elif on_board_parameter == 0:
-            deck = Deck.GetFullDeck()
-            for i in range(0, len(deck)):
-                deck[i] = Card.int_to_str(deck[i])  # fiqure+colour, i.e. Ad - ace diamond
-
-            pair_combinations = list(combinations(deck, 2))  # list of tuples
-            test_probes = random.sample(pair_combinations, len(pair_combinations) // 10)  # 10% for testing
-            training_probes = (list(set(pair_combinations) - set(test_probes)))
-            # print(training_probes[0][0], training_probes[0][1])
-            mc = MonteCarlo(times=10000)  # 'times' says how many times it should play
-            with open("poker-hand-test-" + str(on_board_parameter + 2) + ".data", "w") as test_set:
-                print("Test set")
-                for idx in range(len(test_probes)):
-                    # total_villains - how many oponents
-                    print(idx)
-                    equity = mc.preflop(card1=test_probes[idx][0], card2=test_probes[idx][1], total_villains=4)
-                    string = ','.join(map(str, self.get_ints_from_card_symbols(test_probes[idx][0]) + self.get_ints_from_card_symbols(test_probes[idx][1]) + [equity]))
-                    test_set.write(string + "\n")
-                    test_set.flush()
-
-            with open("poker-hand-training-" + str(on_board_parameter + 2) + ".data", "w") as training_set:
-                print("Test set")
-                for idx in range(len(training_probes)):
-                    # total_villains - how many oponents
-                    print(idx)
-                    equity = mc.preflop(card1=training_probes[idx][0], card2=training_probes[idx][1], total_villains=4)
-                    training_set.write(','.join(map(str, self.get_ints_from_card_symbols(training_probes[idx][0]) + self.get_ints_from_card_symbols(training_probes[idx][1]) + [equity])) + "\n")
-                    training_set.flush()
-
-    def generate_probe(self, on_board_parameter):
+    def generate_probe_3_5(self, on_board_parameter):
         ready_probe = True
         while ready_probe:
             deck = Deck()
@@ -91,25 +70,56 @@ class TrainingSetPreparer:
 
         probe = []
         for card in in_hand + on_board:
-            probe.append(int(math.log2(Card.get_suit_int(card))) + 1)  # colour (library: 1, 2, 4, 8)
-            probe.append(Card.get_rank_int(card) + 1)  # figure (library: 0, 1, 2,...,12)
-
-        # 1: "Royal Straight Flush"
-        # 2: "Straight Flush"
-        # 3: "Four of a Kind"
-        # 4: "Full House"
-        # 5: "Flush"
-        # 6: "Straight"
-        # 7: "Three of a Kind"
-        # 8: "Two Pair
-        # 9: "Pair"
-        # 10: "High Card"
+            probe.append(int(math.log2(Card.get_suit_int(card))) + 1)  # suit (library: 1, 2, 4, 8)
+            probe.append(Card.get_rank_int(card) + 1)  # rank (library: 0, 1, 2,...,12)
 
         # library: 1 - Straight Flush, so must +1
         hand = 1 if hand_strength == 1 else self.evaluator.get_rank_class(hand_strength) + 1
         probe.append(hand)
         probe.append(1-self.evaluator.get_five_card_rank_percentage(hand_strength))
         return ','.join(map(str, probe))  # change list of number to string, where number is separated by ','
+
+    # Prepare files for 4 cases with on_board_parameter: 0, 3, 4, 5
+    # 2 card in hand 0 visible on table (0)
+    # 2 card in hand 3 visible on table (3)
+    # 2 card in hand 4 visible on table (4)
+    # 2 card in hand 5 visible on table (5)
+    def generate_sets(self, on_board_parameter):
+        if all([on_board_parameter >= 3, on_board_parameter <= 5]):
+            # training set
+            self.save_to_file_3_5("training", on_board_parameter, 32768)
+            # test set
+            self.save_to_file_3_5("test", on_board_parameter, 4096)
+
+        elif on_board_parameter == 0:
+            deck = Deck.GetFullDeck()
+            for i in range(0, len(deck)):
+                deck[i] = Card.int_to_str(deck[i])  # rank+suit, i.e. Ad - ace diamond
+
+            pair_combinations = list(combinations(deck, 2))  # list of tuples
+            test_probes = random.sample(pair_combinations, len(pair_combinations) // 10)  # 10% for testing
+            training_probes = (list(set(pair_combinations) - set(test_probes)))
+
+            self.save_to_file_0("training", training_probes)
+            self.save_to_file_0("test", test_probes)
+
+    def save_to_file_0(self, filename_infix, probes):
+        with open("poker-hand-" + filename_infix + "-2.data", "w") as set:
+            mc = MonteCarlo(times=10000)  # 'times' says how many times it should play
+            for idx in range(len(probes)):
+                # total_villains - how many opponents
+                equity = mc.preflop(card1=probes[idx][0], card2=probes[idx][1], total_villains=4)
+                string = ','.join(map(str, self.get_ints_from_card_symbols(
+                    probes[idx][0]) + self.get_ints_from_card_symbols(probes[idx][1]) + [equity]))
+                set.write(string + "\n")
+                set.flush()
+
+    def save_to_file_3_5(self, filename_infix, on_board_parameter, set_size):
+        with open("poker-hand-" + filename_infix + "-" + str(on_board_parameter + 2) + ".data", "w") as set:
+            self.generated_probes = []
+            for idx in range(0, set_size):
+                set.write(self.generate_probe_3_5(on_board_parameter) + "\n")
+                set.flush()
 
     @staticmethod
     def get_ints_from_card_symbols(card):
@@ -119,6 +129,7 @@ class TrainingSetPreparer:
         suit_int = int(math.log2(Card.CHAR_SUIT_TO_INT_SUIT[suit_char])) + 1
         return [suit_int, rank_int]
 
+
 training_set = TrainingSetPreparer()
-TrainingSetPreparer.generate_training_set(training_set, 0)
-#one_hot = OneHot()
+TrainingSetPreparer.generate_sets(training_set, 0)
+# one_hot = OneHot()
