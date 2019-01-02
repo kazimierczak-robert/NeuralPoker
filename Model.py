f8db8d03fa16e801df8d43925e77beb052f1bfa0
kazimierczak-robert <kazimierczak-robert@tlen.pl>
2019-01-02 20:19:59 +0100

    Training model

--- Model.py
+++ Model.py
@@ -9,16 +9,48 @@ class Model:
     def __init__(self, dataset):
         self.dataset = dataset
 
+    def convert_input_to_one_hot(self):
+        columns = None
+
+        with tf.Session() as sess:
+            for ith_column in range(0, self.dataset.on_board*2):
+                column = self.dataset.input[:, ith_column]
+                if ith_column % 2 == 0: # suit
+                    one_hot_column = tf.one_hot(column, 4)
+                    if ith_column == 0:
+                        columns = one_hot_column
+                    else:
+                        columns = tf.concat([columns, one_hot_column], 1)
+                else: # rank
+                    one_hot_column = tf.one_hot(column, 13)
+                    columns = tf.concat([columns, one_hot_column], 1)
+
+        return columns
+
+
     def create(self):
         self.model = keras.models.Sequential()
         # 17 = 13 ranks + 4 suits
-        self.model.add(keras.layers.Dense(7, input_dim=17*self.dataset.on_board, activation=tf.nn.relu))
-        self.model.add(keras.layers.Dense(7, activation=tf.nn.relu))
-        output_1 = keras.layers.Dense(10, activation=tf.nn.softmax)
-        output_2 = keras.layers.Dense(1, activation=tf.nn.sigmoid)
-        output = tf.concat(1, [output_1, output_2])
-        self.model.add(output)
+        input_layer = keras.layers.Input((17*self.dataset.on_board,))
+        second_layer = keras.layers.Dense(7, activation=tf.nn.relu)(input_layer)
+        if self.dataset.on_board == 2:
+            output_layer = keras.layers.Dense(1, activation=tf.nn.sigmoid)(second_layer)
+            self.model = keras.models.Model(input_layer, output_layer)
+        else:
+            output_1 = keras.layers.Dense(11, activation=tf.nn.softmax)(second_layer)
+            output_2 = keras.layers.Dense(1, activation=tf.nn.sigmoid)(second_layer)
+            self.model = keras.models.Model(input_layer, [output_1, output_2])
+
         self.model.compile(optimizer=tf.train.AdamOptimizer(), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
     def train(self):
-        self.model.fit(self.dataset.input, self.dataset.output, epochs=150)
+        if self.dataset.on_board == 2:
+            self.model.fit(self.convert_input_to_one_hot(), self.dataset.output, epochs=150, steps_per_epoch=self.dataset.number_of_samples)
+        else:
+            self.model.fit(self.convert_input_to_one_hot(), [self.dataset.output[:,0], self.dataset.output[:,1]], epochs=1, steps_per_epoch=self.dataset.number_of_samples)
+
+
+importer = SetImporter("Sets\poker-hand-training-5.data", 5)
+model = Model(importer)
+model.create()
+model.train()
--- SetImporter.py
+++ SetImporter.py
@@ -3,12 +3,14 @@ import numpy as np
 
 class SetImporter:
     input: np.ndarray
+    number_of_samples: int
     on_board: int
     output: np.ndarray
 
     def __init__(self, file_name, on_board):
         self.on_board = on_board
         self.divide_dataset(np.loadtxt(file_name, delimiter=','))
+        self.number_of_samples = self.input.shape[0]
 
     def divide_dataset(self, dataset):
         self.input = dataset[:, 0:self.on_board*2]
