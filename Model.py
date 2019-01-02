538dbaadded6dc1832a25863387077764b488ef9
kazimierczak-robert <kazimierczak-robert@tlen.pl>
2019-01-03 00:08:44 +0100

    Testing, saving, loading model

--- Model.py
+++ Model.py
@@ -9,12 +9,12 @@ class Model:
     def __init__(self, dataset):
         self.dataset = dataset
 
-    def convert_input_to_one_hot(self):
+    def convert_input_to_one_hot(self, input):
         columns = None
 
         with tf.Session() as sess:
             for ith_column in range(0, self.dataset.all_cards*2):
-                column = self.dataset.input[:, ith_column]
+                column = input[:, ith_column]
                 if ith_column % 2 == 0: # suit
                     one_hot_column = tf.one_hot(column, 4)
                     if ith_column == 0:
@@ -45,12 +45,29 @@ class Model:
 
     def train(self):
         if self.dataset.all_cards == 2:
-            self.model.fit(self.convert_input_to_one_hot(), self.dataset.output, epochs=150, steps_per_epoch=self.dataset.number_of_samples)
+            self.model.fit(self.convert_input_to_one_hot(self.dataset.input), self.dataset.output, epochs=150, steps_per_epoch=self.dataset.number_of_samples)
         else:
-            self.model.fit(self.convert_input_to_one_hot(), [self.dataset.output[:,0], self.dataset.output[:,1]], epochs=1, steps_per_epoch=self.dataset.number_of_samples)
+            self.model.fit(self.convert_input_to_one_hot(self.dataset.input), [self.dataset.output[:,0], self.dataset.output[:,1]], epochs=1, steps_per_epoch=self.dataset.number_of_samples)
 
+    def save(self, output_file_name):
+        with open(output_file_name + ".json", "w") as json_file:
+            json_file.write(self.model.to_json())
+        self.model.save_weights(output_file_name + "model.h5")
 
-importer = SetImporter("Sets\poker-hand-training-5.data", 5)
-model = Model(importer)
+    def load(self, input_file_name):
+        with open(input_file_name + ".json", "r") as json_file:
+            self.model = tf.keras.models.model_from_json(json_file.read())
+        self.model.load_weights(input_file_name + "model.h5")
+
+    def test(self, test_set: SetImporter):
+        if test_set.all_cards == 2:
+            print(self.model.evaluate(self.convert_input_to_one_hot(test_set.input), test_set.output, steps=test_set.number_of_samples))
+        else:
+            print(self.model.evaluate(self.convert_input_to_one_hot(test_set.input), [test_set.output[:,0], test_set.output[:,1]], steps=test_set.number_of_samples))
+
+train_test = SetImporter("Sets\poker-hand-training-5.data", 5)
+test_test = SetImporter("Sets\poker-hand-test-5.data", 5)
+model = Model(train_test)
 model.create()
 model.train()
+model.test(test_test)
