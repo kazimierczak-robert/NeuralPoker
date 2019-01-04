abc5662a213023150c6311095df636f39afd81e7
gradzka <monika.gradzka@tlen.pl>
2019-01-04 16:31:13 +0100

    SetGenerator refactoring

--- SetGenerator.py
+++ SetGenerator.py
@@ -4,7 +4,8 @@ import math
 from Libraries.MonteCarlo.Monte_Carlo_Poker_Simulation import MonteCarlo
 import os
 import random
-import time
+import itertools
+
 
 class SetGenerator:
     # Hands:
@@ -35,22 +36,27 @@ class SetGenerator:
     dir_path = "./Sets"
     evaluator = Evaluator()
 
-    # Prepare files for 4 cases with on_board_parameter: 0, 3, 4, 5
-    # 2 card in hand 0 visible on table (0)
-    # 2 card in hand 3 visible on table (3)
-    # 2 card in hand 4 visible on table (4)
-    # 2 card in hand 5 visible on table (5)
-    def generate_sets(self, on_board_parameter):
+    # Prepare files for 4 cases with all_cards_no: 2, 5, 6, 7
+    # 2 card in hand 0 visible on table (2)
+    # 2 card in hand 3 visible on table (5)
+    # 2 card in hand 4 visible on table (6)
+    # 2 card in hand 5 visible on table (7)
+    def generate_sets(self, all_cards_no):
         if not os.path.exists(self.dir_path):
             os.makedirs(self.dir_path)
-        if all([on_board_parameter >= 3, on_board_parameter <= 5]):
-            deck = Deck.GetFullDeck()
-            for i in range(0, len(deck)):
-                deck[i] = Card.int_to_str(deck[i])  # rank+suit, i.e. Ad - ace diamond
 
-            pair_combinations = list(combinations(deck, 2))  # list of tuples
+        deck = Deck.GetFullDeck()
+        for i in range(0, len(deck)):
+            # convert int to string: rank+suit, i.e. Ad - ace diamond
+            deck[i] = Card.int_to_str(deck[i])
+        # list of tuples
+        pair_combinations = list(combinations(deck, 2))
+
+        training_probes = []
+        test_probes = []
+        if all([all_cards_no >= 5, all_cards_no <= 7]):
+
             all_probes = list()
-
             for pair_combination in pair_combinations:
                 pair = list(pair_combination)
                 deck_without_pair = deck.copy()
@@ -60,137 +66,68 @@ class SetGenerator:
                 for i in range(0, 30):
                     pair_and_on_board = pair.copy()
                     deck_without_pair_and_on_board = deck_without_pair.copy()
-                    for j in range(0, on_board_parameter):
-                        pair_and_on_board.append(deck_without_pair_and_on_board.pop(deck_without_pair_and_on_board.index(
-                            random.choice(deck_without_pair_and_on_board))))
+                    for j in range(0, all_cards_no - 2):
+                        pair_and_on_board.append(deck_without_pair_and_on_board.pop(
+                            deck_without_pair_and_on_board.index(
+                                random.choice(deck_without_pair_and_on_board))))
                     all_probes.append(tuple(pair_and_on_board))
 
             test_probes = random.sample(all_probes, len(all_probes) // 10)  # 10% for testing
             training_probes_set = set(all_probes) - set(test_probes)
             training_probes = (list(training_probes_set))
 
-            if on_board_parameter == 3:
-                self.save_to_file_3("training", training_probes)
-                self.save_to_file_3("test", test_probes)
-            elif on_board_parameter == 4:
-                self.save_to_file_4("training", training_probes)
-                self.save_to_file_4("test", test_probes)
-            elif on_board_parameter == 5:
-                self.save_to_file_5("training", training_probes)
-                self.save_to_file_5("test", test_probes)
-
-        elif on_board_parameter == 0:
-            deck = Deck.GetFullDeck()
-            for i in range(0, len(deck)):
-                deck[i] = Card.int_to_str(deck[i])  # rank+suit, i.e. Ad - ace diamond
-
-            pair_combinations = list(combinations(deck, 2))  # list of tuples
+        elif all_cards_no == 2:
             test_probes = random.sample(pair_combinations, len(pair_combinations) // 10)  # 10% for testing
             training_probes = (list(set(pair_combinations) - set(test_probes)))
 
-            self.save_to_file_0("training", training_probes)
-            self.save_to_file_0("test", test_probes)
+        self.save_to_file(all_cards_no, 2500, training_probes, "training")
+        self.save_to_file(all_cards_no, 2500, test_probes, "test")
 
-    def save_to_file_0(self, filename_infix, probes):
-        with open(self.dir_path + "/poker-hand-" + filename_infix + "-2.data", "w") as set:
-            mc = MonteCarlo(times=10000)  # 'times' says how many times it should play
-            for idx in range(len(probes)):
-                # total_villains - how many opponents
-                equity = mc.preflop(card1=probes[idx][0], card2=probes[idx][1], total_villains=4)
-                string = ','.join(map(str, self.get_ints_from_card_symbols(
-                    probes[idx][0]) + self.get_ints_from_card_symbols(probes[idx][1]) + [equity]))
-                set.write(string + "\n")
-                set.flush()
-
-    def save_to_file_3(self, filename_infix, probes):
-        with open(self.dir_path + "/poker-hand-" + filename_infix + "-5.data", "w") as set:
-            mc = MonteCarlo(times=10000)  # 'times' says how many times it should play
+    def save_to_file(self, all_cards_no, monte_carlo_rounds, probes, training_or_test):
+        with open("{}/poker-hand-{}-{}.data".format(self.dir_path, training_or_test, all_cards_no), "w") as set:
+            mc = MonteCarlo(times=monte_carlo_rounds)  # 'times' says how many times it should play
             for idx in range(len(probes)):
                 print(idx)
-                # start_time = time.time()
                 probe = list(probes[idx])
-                cards = list()
-                board = list()
-                for c in probe[0:2]:
-                    cards.append(Card.new(c))
-                for c in probe[2:]:
-                    board.append(Card.new(c))
-
-                hand_strength = self.evaluator.evaluate(cards, board)
-                # library: 0 - Straight Flush
-                hand = 0 if hand_strength == 1 else self.evaluator.get_rank_class(hand_strength)
-
                 # total_villains - how many opponents
-                equity = mc.flop(card1=probe[0], card2=probe[1], fcard1=probe[2], fcard2=probe[3], fcard3=probe[4], total_villains=4)
-                string = ','.join(map(str,
-                                      self.get_ints_from_card_symbols(probe[0]) +
-                                      self.get_ints_from_card_symbols(probe[1]) +
-                                      self.get_ints_from_card_symbols(probe[2]) +
-                                      self.get_ints_from_card_symbols(probe[3]) +
-                                      self.get_ints_from_card_symbols(probe[4]) + [hand] + [equity]))
-                set.write(string + "\n")
-                set.flush()
-                # print("--- %s seconds ---" % (time.time() - start_time))
+                if all_cards_no == 2:
+                    equity = mc.preflop(card1=probe[0], card2=probe[1], total_villains=4)
+                    set.write("{}\n".format(','.join(map(str, list(itertools.chain.from_iterable(
+                        map(self.get_ints_from_card_symbols, probe))) + [equity]))))
 
-    def save_to_file_4(self, filename_infix, probes):
-        with open(self.dir_path + "/poker-hand-" + filename_infix + "-6.data", "w") as set:
-            mc = MonteCarlo(times=10000)  # 'times' says how many times it should play
-            for idx in range(len(probes)):
-                probe = list(probes[idx])
-                cards = list()
-                board = list()
-                for c in probe[0:2]:
-                    cards.append(Card.new(c))
-                for c in probe[2:]:
-                    board.append(Card.new(c))
+                elif all([all_cards_no >= 5, all_cards_no <= 7]):
+                    hand_cards, board_cards = self.convert_and_divide_string_probe_to_ints(probe)
+                    hand_strength = self.evaluator.evaluate(hand_cards, board_cards)
+                    # 0 - Straight Flush
+                    hand = 0 if hand_strength == 1 else self.evaluator.get_rank_class(hand_strength)
 
-                hand_strength = self.evaluator.evaluate(cards, board)
-                # library: 0 - Straight Flush
-                hand = 0 if hand_strength == 1 else self.evaluator.get_rank_class(hand_strength)
-
-                # total_villains - how many opponents
-                equity = mc.turn(card1=probe[0], card2=probe[1], fcard1=probe[2],
-                                 fcard2=probe[3], fcard3=probe[4], tcard=probe[5], total_villains=4)
-                string = ','.join(map(str,
-                                      self.get_ints_from_card_symbols(probe[0]) +
-                                      self.get_ints_from_card_symbols(probe[1]) +
-                                      self.get_ints_from_card_symbols(probe[2]) +
-                                      self.get_ints_from_card_symbols(probe[3]) +
-                                      self.get_ints_from_card_symbols(probe[4]) +
-                                      self.get_ints_from_card_symbols(probe[5]) + [hand] + [equity]))
-                set.write(string + "\n")
+                    if all_cards_no == 5:
+                        equity = mc.flop(card1=probe[0], card2=probe[1], fcard1=probe[2], fcard2=probe[3],
+                                         fcard3=probe[4], total_villains=4)
+                    elif all_cards_no == 6:
+                        equity = mc.turn(card1=probe[0], card2=probe[1], fcard1=probe[2],
+                                         fcard2=probe[3], fcard3=probe[4], tcard=probe[5], total_villains=4)
+                    elif all_cards_no == 7:
+                        equity = mc.river(card1=probe[0], card2=probe[1], fcard1=probe[2],
+                                          fcard2=probe[3], fcard3=probe[4], tcard=probe[5], rcard=probe[6],
+                                          total_villains=4)
+                    set.write("{}\n".format(','.join(map(str, list(itertools.chain.from_iterable(
+                        map(self.get_ints_from_card_symbols, probe))) + [hand] + [equity]))))
                 set.flush()
 
-    def save_to_file_5(self, filename_infix, probes):
-        with open(self.dir_path + "/poker-hand-" + filename_infix + "-7.data", "w") as set:
-            mc = MonteCarlo(times=10000)  # 'times' says how many times it should play
-            for idx in range(len(probes)):
-                probe = list(probes[idx])
-                cards = list()
-                board = list()
-                for c in probe[0:2]:
-                    cards.append(Card.new(c))
-                for c in probe[2:]:
-                    board.append(Card.new(c))
+    @staticmethod
+    def convert_and_divide_string_probe_to_ints(self, probe):
+        hand_cards = list()
+        for card in probe[0:2]:
+            # card in string format to integer format
+            hand_cards.append(Card.new(card))
 
-                hand_strength = self.evaluator.evaluate(cards, board)
-                # library: 0 - Straight Flush
-                hand = 0 if hand_strength == 1 else self.evaluator.get_rank_class(hand_strength)
-
-                # total_villains - how many opponents
-                equity = mc.river(card1=probe[0], card2=probe[1], fcard1=probe[2],
-                                 fcard2=probe[3], fcard3=probe[4], tcard=probe[5], rcard=probe[6], total_villains=4)
-                string = ','.join(map(str,
-                                      self.get_ints_from_card_symbols(probe[0]) +
-                                      self.get_ints_from_card_symbols(probe[1]) +
-                                      self.get_ints_from_card_symbols(probe[2]) +
-                                      self.get_ints_from_card_symbols(probe[3]) +
-                                      self.get_ints_from_card_symbols(probe[4]) +
-                                      self.get_ints_from_card_symbols(probe[5]) +
-                                      self.get_ints_from_card_symbols(probe[6]) + [hand] + [equity]))
-                set.write(string + "\n")
-                set.flush()
-
+        board_cards = list()
+        for card in probe[2:]:
+            # card in string format to integer format
+            board_cards.append(Card.new(card))
+        return hand_cards, board_cards
+    
     @staticmethod
     def get_ints_from_card_symbols(card):
         rank_char = card[0]
--- Source.py
+++ Source.py
@@ -3,7 +3,7 @@ from SetGenerator import SetGenerator
 from SetImporter import SetImporter
 
 training_set = SetGenerator()
-training_set.generate_sets(3)  # on board
+training_set.generate_sets(5)  # on board
 
 # train_test = SetImporter("Sets\poker-hand-training-5.data", 5)
 # test_test = SetImporter("Sets\poker-hand-test-5.data", 5)
