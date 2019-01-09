3b1b25c53086f979683aec9e580fc4f78aa83cc9
gradzka <monika.gradzka@tlen.pl>
2019-01-09 21:13:23 +0100

    Add GeneratedModels and GeneratedSets, Source with example to run, add jupyter file

--- Model/Model.py
+++ Model/Model.py
@@ -17,6 +17,29 @@ class Model:
         self.model = None
         self.all_cards = self.dataset.all_cards if dataset is not None else 0
 
+    # https://machinelearningmastery.com/custom-metrics-deep-learning-keras-python/
+    # Keras Regression metrics:
+    # - Mean Squared Error
+    # - Mean Absolute Error
+    # - Mean Absolute Percentage Error
+    # - Cosine Proximity
+    # Keras Classification Metrics
+    # - Binary Accuracy
+    # - Categorical Accuracy
+    # - Sparse Categorical Accuracy
+    # - Top k Categorical Accuracy
+    # - Sparse Top k Categorical Accuracy
+    def compile(self):
+        if self.all_cards == 2:
+            # metrics for regression
+            self.model.compile(optimizer=tf.train.AdamOptimizer(), loss='mse',
+                               metrics=['mse'])
+        else:
+            # metrics for classification and regression
+            self.model.compile(optimizer=tf.train.AdamOptimizer(),
+                               loss={'output_layer_1': 'sparse_categorical_crossentropy', 'output_layer_2': 'mse'},
+                               metrics={'output_layer_1': 'acc', 'output_layer_2': 'mse'})
+
     def convert_input_to_one_hot(self, input, all_cards):
         columns = None
 
@@ -52,65 +75,12 @@ class Model:
                 self.model = keras.models.Model(input_layer, [output_1, output_2])
             self.compile()
 
-    def train(self):
-        if all([self.dataset is not None, self.model is not None]):
-            # verbose = 0 - don't print
-            if self.dataset.all_cards == 2:
-                self.model.fit(self.convert_input_to_one_hot(self.dataset.input, self.dataset.all_cards),
-                               self.dataset.output, epochs=15, steps_per_epoch=self.dataset.number_of_samples, verbose=0)
-            else:
-                self.model.fit(self.convert_input_to_one_hot(self.dataset.input, self.dataset.all_cards),
-                               [self.dataset.output[:, 0], self.dataset.output[:, 1]], epochs=1,
-                               steps_per_epoch=self.dataset.number_of_samples, verbose=0)
-
-    def save(self, output_file_name):
-        if not os.path.exists(self.dir_path):
-            os.makedirs(self.dir_path)
-
-        with open(output_file_name + ".json", "w") as json_file:
-            json_file.write(self.model.to_json())
-        self.model.save_weights(output_file_name + ".h5")
-
     def load(self, input_file_name, all_cards_no):
         with open(input_file_name + ".json", "r") as json_file:
             self.model = tf.keras.models.model_from_json(json_file.read())
         self.model.load_weights(input_file_name + ".h5")
         self.all_cards = all_cards_no
         self.compile()
-
-    # https://machinelearningmastery.com/custom-metrics-deep-learning-keras-python/
-    # Keras Regression metrics:
-    # - Mean Squared Error
-    # - Mean Absolute Error
-    # - Mean Absolute Percentage Error
-    # - Cosine Proximity
-    # Keras Classification Metrics
-    # - Binary Accuracy
-    # - Categorical Accuracy
-    # - Sparse Categorical Accuracy
-    # - Top k Categorical Accuracy
-    # - Sparse Top k Categorical Accuracy
-    def compile(self):
-        if self.all_cards == 2:
-            # metrics for regression
-            self.model.compile(optimizer=tf.train.AdamOptimizer(), loss='mse',
-                               metrics=['mse'])
-        else:
-            # metrics for classification and regression
-            self.model.compile(optimizer=tf.train.AdamOptimizer(),
-                               loss={'output_layer_1': 'sparse_categorical_crossentropy', 'output_layer_2': 'mse'},
-                               metrics={'output_layer_1': 'acc', 'output_layer_2': 'mse'})
-
-    def test(self, test_set: SetImporter):
-        if test_set.all_cards == 2:
-            eval_result = self.model.evaluate(self.convert_input_to_one_hot(test_set.input, test_set.all_cards),
-                                              test_set.output, steps=test_set.number_of_samples, verbose=0)
-        else:
-            eval_result = self.model.evaluate(self.convert_input_to_one_hot(test_set.input, test_set.all_cards),
-                                              [test_set.output[:, 0], test_set.output[:, 1]],
-                                              steps=test_set.number_of_samples, verbose=1)
-        for idx in range(0, len(self.model.metrics_names)):
-            print(self.model.metrics_names[idx] + ":", eval_result[idx])
 
     def predict(self, input: list):
         b = SetGenerator.convert_char_input_to_int_list(input)
@@ -121,3 +91,33 @@ class Model:
         elif all([len(input) >= 5, len(input) <= 7]):
             return np.argmax(results[0][0]), results[1][0][0]
         return None
+
+    def save(self, output_file_name):
+        if not os.path.exists(self.dir_path):
+            os.makedirs(self.dir_path)
+
+        with open(output_file_name + ".json", "w") as json_file:
+            json_file.write(self.model.to_json())
+        self.model.save_weights(output_file_name + ".h5")
+
+    def test(self, test_set: SetImporter):
+        if test_set.all_cards == 2:
+            eval_result = self.model.evaluate(self.convert_input_to_one_hot(test_set.input, test_set.all_cards),
+                                              test_set.output, steps=test_set.number_of_samples, verbose=0)
+        else:
+            eval_result = self.model.evaluate(self.convert_input_to_one_hot(test_set.input, test_set.all_cards),
+                                              [test_set.output[:, 0], test_set.output[:, 1]],
+                                              steps=test_set.number_of_samples, verbose=0)
+        for idx in range(0, len(self.model.metrics_names)):
+            print(self.model.metrics_names[idx] + ":", eval_result[idx])
+
+    def train(self):
+        if all([self.dataset is not None, self.model is not None]):
+            # verbose = 0 - don't print
+            if self.dataset.all_cards == 2:
+                self.model.fit(self.convert_input_to_one_hot(self.dataset.input, self.dataset.all_cards),
+                               self.dataset.output, epochs=15, steps_per_epoch=self.dataset.number_of_samples, verbose=0)
+            else:
+                self.model.fit(self.convert_input_to_one_hot(self.dataset.input, self.dataset.all_cards),
+                               [self.dataset.output[:, 0], self.dataset.output[:, 1]], epochs=1,
+                               steps_per_epoch=self.dataset.number_of_samples, verbose=0)
--- Source.py
+++ Source.py
@@ -3,20 +3,21 @@
 # generator = SetGenerator()
 # generator.generate_sets(2)
 
-from Sets.SetImporter import SetImporter
-from Sets.SetGenerator import SetGenerator
+# from Sets.SetImporter import SetImporter
+# from Sets.SetGenerator import SetGenerator
 from Model.Model import Model
 
-all_cards_no = 5
-train_set = SetImporter("{}/poker-hand-{}-{}.data".format(SetGenerator.dir_path, "training", all_cards_no), all_cards_no)
-test_set = SetImporter("{}/poker-hand-{}-{}.data".format(SetGenerator.dir_path, "test", all_cards_no), all_cards_no)
+all_cards_no = 2
+# train_set = SetImporter("{}/poker-hand-{}-{}.data".format(SetGenerator.dir_path, "training", all_cards_no),
+# all_cards_no)
+# test_set = SetImporter("{}/poker-hand-{}-{}.data".format(SetGenerator.dir_path, "test", all_cards_no), all_cards_no)
 
-model = Model(train_set)
-# for neurons_in_hidden_layer in range(8, 32, 8):
-print("Result for {} neurons in hidden layer".format(28))
-# model.create(28)
-model.load("{}/{}{}".format(Model.dir_path, "model-", all_cards_no), all_cards_no)
+# model = Model(train_set)
+model = Model()
+# neurons_in_hidden_layer = 28
+# model.create(neurons_in_hidden_layer)
+model.load("{}/model-{}".format(Model.dir_path, all_cards_no), all_cards_no)
 # model.train()
-# model.save("{}/{}{}".format(Model.dir_path, "model-", all_cards_no))
+# model.save("{}/model-{}".format(Model.dir_path, all_cards_no))
 # model.test(test_set)
 print(model.predict(['5s', '5d', 'Td', 'Ts', 'Qs']))
